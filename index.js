1fe4ddde67a56c57c8eaa1ca827959928c415fee
ryan10599 <thanapol.ryan@mail.kmutt.ac.th>
2021-03-27 12:52:22 +0700

    [EDIT] fix syntax for query and update statement

--- index.js
+++ index.js
@@ -26,13 +26,12 @@ app.get("/test", (req, res) => {
 
 app.get("/l/:refUrl", async (req, res) => {
   let refUrl = req.params.refUrl;
-  (await db).execute(
-    `update url set visits = visits+1 where short_url = '${refUrl}'`
+  await db.execute(
+    "update url set visits = visits+1 where short_url = ?", [refUrl]
   );
   const [rows] = await db.execute(
-    `SELECT full_url FROM url WHERE short_url = '${refUrl}'`
+    'SELECT full_url FROM url WHERE short_url = ?', [refUrl]
   );
-
   let fullUrl;
   try {
     fullUrl = rows[0].full_url;
@@ -56,13 +55,13 @@ app.post("/link", async (req, res, next) => {
     return result;
   }
   const [rows] = await db.execute(
-    `SELECT short_url FROM url WHERE full_url = '${fullUrl}'`
+    "SELECT short_url FROM url WHERE full_url = ?",[fullUrl]
   );
   if (rows.length == 0) {
     let preRandom = randomId(4);
     try {
       await db.execute(
-        `INSERT INTO url (full_url, short_url) VALUES ('${fullUrl}','${preRandom}')`
+        "INSERT INTO url (full_url, short_url) VALUES (?,?)", [fullUrl, preRandom]
       );
       return res.json({
         link: `http://${process.env.APP_URL}/l/${preRandom}`,
@@ -70,15 +69,16 @@ app.post("/link", async (req, res, next) => {
     } catch (error) {
       preRandom = randomId(5);
       await db.execute(
-        `INSERT INTO url (full_url, short_url) VALUES ('${fullUrl}','${preRandom}')`
+        "INSERT INTO url (full_url, short_url) VALUES (?,?)", [fullUrl, preRandom]
       );
       return res.json({
         link: `http://${process.env.APP_URL}/l/${preRandom}`,
       });
     }
   }else{
+    let short_url = rows[0].short_url;
     return res.json({
-        link: `http://${process.env.APP_URL}/l/${rows[0].short_url}`,
+        link: `http://${process.env.APP_URL}/l/${short_url}`,
       });
   }
 });
@@ -87,7 +87,7 @@ app.get("/l/:refUrl/stats", async (req, res) => {
   let refUrl = req.params.refUrl;
 
   const [rows] = await db.execute(
-    `SELECT visits FROM url WHERE short_url = '${refUrl}'`
+    "SELECT visits FROM url WHERE short_url = ?", [refUrl]
   );
   return res.json({
     visit: rows[0].visits,
